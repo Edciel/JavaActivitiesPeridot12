a7f71d3bac51d0cdbcad609ba93f073392a0fe6e
Edciel <dayaonelvie@gmail.com>
2026-06-17 13:08:33 +0800

    this is my first commit in java

---
+++ basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,18 @@
+package basic_Structure_and_Syntax;
+// this is a simple Java program that prints "Hello, World!" to the console
+public class HelloWorld {
+    // the main method is the entry point of the program
+    // it is where the program starts executing
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Im a Edciel D. Geolina!");
+        System.out.println("Im a student of Grade 12-Peridot!");
+        System.out.println("Im from Caloocan City!");
+    }
+}
+                
+
+
+
+
+
