7653d6d2715b56f662e5e04214f7a32b56835458
Edciel <dayaonelvie@gmail.com>
2026-06-18 14:42:38 +0800

    jun 18 2026

---
+++ basic_Structure_and_Syntax/variableAcitivity.java
@@ -0,0 +1,23 @@
+package basic_Structure_and_Syntax;
+
+public class variableAcitivity {
+    public static void main(String[] args) {
+     String Animalname = "pig";
+    int animalage = 32;
+    final int animalweight = 100;
+    String animalbreed = "Yorkshire";
+        System.out.println("Animal Name: " + Animalname);
+        System.out.println("Animal Age: " + animalage);
+        System.out.println("Animal Weight: " + animalweight);
+        animalbreed = "Landrace";
+        System.out.println("Animal Breed: " + animalbreed);
+        animalage = 33; //reassigning the value of animalage
+        System.out.println("Animal Age: " + animalage);
+        float animalheight = 8.5f; //declaring and initializing a float
+        System.out.println("Animal Height: " + animalheight);
+        char animalsection = 'A';
+         System.out.println("Animal Section: " + animalsection);
+         boolean isDomestic = true;
+          System.out.println("Is Domestic: " + isDomestic);
+    }
+}
---
+++ basic_Structure_and_Syntax/variables.java
@@ -0,0 +1,22 @@
+package basic_Structure_and_Syntax;
+
+public class variables {
+public static void main(String[] args) {
+    String name = "Edciel D. Geolina";
+    int age = 17; //no double qoute for number(int)
+    final int score = 10;
+    String subject; //declaring variables without initializing
+    System.out.println("Name: " + name);
+    System.out.println("Age:" + age);
+    System.out.println("score:" + score);
+    age = 18; //reassining the value of age
+    System.out.println("Age: " + age);
+    float height = 5.8f; //declaring and initializing a float
+    System.out.println("height; " + height);
+    char section = 'P';
+     System.out.println("section; " + section);
+     boolean regularStudent = true;
+      System.out.println("Regular Student; " + regularStudent);
+         }
+
+     }
